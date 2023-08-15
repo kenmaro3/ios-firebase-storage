098170a505c5f9b8b144c231ff982a0533b464f6
kenmaro3 <miha.ken.19@gmail.com>
2023-08-15 11:41:53 +0900

    Initial Commit

---
+++ FirebaseStorageIos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseStorageIos
+//
+//  Created by Kentaro Mihara on 2023/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseStorageIos/FirebaseStorageIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseStorageIosApp.swift
+//  FirebaseStorageIos
+//
+//  Created by Kentaro Mihara on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseStorageIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
